db950ccf3f6905779e7998d7414b6679f0d22207
Mojtaba Sabbagh <mojtaba.sabbagh@yahoo.com>
2022-08-16 23:36:29 +0430

    Static admin resolved.

--- telbook/settings.py
+++ telbook/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
--- telbook/urls.py
+++ telbook/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('tel/admin/', admin.site.urls),
     path('tel/', include('telephones.urls')),
 ]
